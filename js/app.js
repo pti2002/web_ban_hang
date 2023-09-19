69547e558aaf79d75275d32d36b706a8210d25b9
pti2002 <145414043+pti2002@users.noreply.github.com>
2023-09-19 03:45:07 +0000

    Pending changes exported from your codespace

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(document).ready(function(){
+	$('#eye').click(function(){
+		$(this).toggleClass('open');
+		$(this).children('i').toggleClass('fa-eye-slash fa-eye');
+		if($(this).hasClass('open')){
+			$(this).prev().attr('type', 'text');
+		}else{
+			$(this).prev().attr('type', 'password');
+		}
+	});	
+});
+
